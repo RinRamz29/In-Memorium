69a81d210e2787414acddedab80fe0058641b125
Ruben Arias <zap483@amerike.edu.mx>
2025-03-05 14:49:05 -0600

    PlatformFall

--- Assets/_Memoriam/Script/Plataformas/FallinPlatforms.cs
+++ Assets/_Memoriam/Script/Plataformas/FallinPlatforms.cs
@@ -1,16 +1,42 @@
+using DG.Tweening;
 using UnityEngine;
+using System.Threading.Tasks;
 
-public class FallinPlatforms : MonoBehaviour
+public class FallinPlatforms : MonoBehaviour, IPlatforms
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float _moveTime = 1f;
+    [SerializeField] private Ease ease = Ease.InOutQuad;
+
+    [SerializeField] private Vector2 pointA = Vector2.zero;
+
+    [SerializeField] private float waitTime = 1.5f;
+
+    private Vector2 _startPosition;  
+
     void Start()
     {
-        
+        _startPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private async void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            await PlatformFall();
+        }
+    }
+
+    private async Task PlatformFall()
+    {
+        await Task.Delay(3000);
+        Move();
+    }
+
+    public void Move()
+    {
+        DOTween.Sequence()
+            .Append(transform.DOMove(_startPosition + pointA, _moveTime).SetEase(ease))
+            .AppendInterval(waitTime)
+            .Append(transform.DOMove(_startPosition, _moveTime).SetEase(ease));
     }
 }
