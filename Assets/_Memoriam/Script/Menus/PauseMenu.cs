b7ca2a6cc98c6bf4d59cc10b004fe18ad36dab6c
JhinSanchez <manoloserrano2010@hotmail.com>
2025-03-06 15:24:34 -0600

    PauseMenu hecho

--- Assets/_Memoriam/Script/Menus/PauseMenu.cs
+++ Assets/_Memoriam/Script/Menus/PauseMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using _Memoriam.Script.Managers;
 using UnityEngine;
 using Zenject;
@@ -6,18 +7,44 @@ namespace _Memoriam.Script.Menus
 {
     public class PauseMenu : MonoBehaviour
     {
+        public GameObject pause;
         public GameObject pauseMenu;
+        public GameObject LoadMenu;
         [SerializeField] private SceneDataBase sceneData;
         
         public void Resume()
         {
-            pauseMenu.SetActive(false);    
-            GameStateManager.Instance.OnGameStateChanged?.Invoke(GameStateManager.GameState.OnGameplay);
+                pauseMenu.SetActive(false);
+                pause.SetActive(false);
+                GameStateManager.Instance.OnGameStateChanged?.Invoke(GameStateManager.GameState.OnGameplay);
+            
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                pauseMenu.SetActive(true);
+                pause.SetActive(true);
+                GameStateManager.Instance.OnGameStateChanged?.Invoke(GameStateManager.GameState.OnPause);
+            }
         }
 
         public async void Menu()
         {
             await UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneData.LoadingSceneName);
+        }
+
+        public void LoadGame()
+        {
+            pause.SetActive(false);
+            LoadMenu.SetActive(true);
+        }
+
+        public void pauseMenuLoad()
+        {
+            pause.SetActive(true);
+            LoadMenu.SetActive(false);
         }
     }
 }
