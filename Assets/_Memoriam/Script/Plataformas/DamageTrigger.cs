8fd62012e3f831ddc99c731948636a68f50827d8
rofotrofo <j.cruzp02@gmail.com>
2025-03-06 22:49:13 -0600

    sistema de daño
    el jugador recibe daño diferenciando entre lava (muerte instantánea) y pinchos (daño x) con un espacio más para futuros proyectiles

--- Assets/_Memoriam/Script/Plataformas/DamageTrigger.cs
+++ Assets/_Memoriam/Script/Plataformas/DamageTrigger.cs
@@ -6,14 +6,34 @@ namespace _Memoriam.Script.Plataformas
     public class DamageTrigger : MonoBehaviour
     {
         [SerializeField] private float damage;
+        [SerializeField] private COSA tipoDedanio;
         
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.TryGetComponent<IPlayer>(out var player))
             {
                 Debug.Log("Player received " + damage + " damage");
-                player.ReceiveDamage(damage);
+                Debug.Log("Player received " + damage + " damage");
+
+                switch (tipoDedanio)
+                {
+                    case COSA.PINCHOS:
+                        player.ReceiveDamage(damage); // Hace daño normal
+                        break;
+
+                    case COSA.LAVA:
+                        player.ReceiveDamage(9999); // Muerte instantánea
+                        Debug.Log("Player died from LAVA");
+                        break;
+                }
             }
         }
     }
+}
+
+enum COSA
+{
+    PINCHOS,
+    LAVA,
+    PROYECTIL
 }
