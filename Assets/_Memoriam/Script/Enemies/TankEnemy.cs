6427dd49d1b543bc7d5a6d797736105ee8bf06b9
JhinSanchez <manoloserrano2010@hotmail.com>
2025-03-21 11:47:10 -0600

    copia al primer nivel

---
+++ Assets/_Memoriam/Script/Enemies/TankEnemy.cs
@@ -0,0 +1,105 @@
+using _Memoriam.Script.Enemies;
+using _Memoriam.Script.Enemies.BT;
+using _Memoriam.Script.General;
+using _Memoriam.Script.Managers;
+using UnityEngine;
+
+public class TankEnemy : BaseEnemy
+{
+    private Parallel _behaviourTree;
+        
+        private void Awake()
+        {
+            Health = MaxHealth;
+            LastAttackTime = -AttackTimeOut;
+            InitialAttackTimer = -AttackTimeOut;
+            SetUpBehaviorSelector();
+        }
+
+        private void Start()
+        {
+            PatrolPoints.Add(transform.position);
+            foreach (var offset in OffsetPoints)
+            {
+                var offsetX = (transform.position.x + offset.x);
+                var offsetY = (transform.position.y + offset.y);
+                PatrolPoints.Add(new Vector2(offsetX, offsetY));
+            }
+        }
+
+        private void OnEnable()
+        {
+            GameStateManager.Instance.OnGameStateChanged += OnStateChanged;
+        }
+
+        private void SetUpBehaviorSelector()
+        {
+            _behaviourTree = new Parallel("BaseEnemySelector");
+            
+            // Continuous Detection Branch
+            var detectionLeaf = new Leaf("ContinuousDetection", new Stretegies.ActionStrategy(Detect), 3);
+            _behaviourTree.AddChild(detectionLeaf);
+            
+            // Main Behavior Branch
+            var behaviorSelector = new PrioritySelector("BehaviorSelector");
+            
+            // Chase Logic
+            var chaseSequence = new Sequence("ChaseSequence");
+            chaseSequence.AddChild(new Leaf("CheckIfDetected", new Stretegies.Condition(() => 
+                EnemyDetected), 2));
+            chaseSequence.AddChild(new Leaf("MoveTowardsEnemy", new Stretegies.ActionStrategy(MoveTowards), 2));
+            chaseSequence.AddChild(new Leaf("Attack", new Stretegies.ActionStrategy(Attack), 2));  
+            behaviorSelector.AddChild(chaseSequence);
+             
+            // Return to Home Logic
+            var returnHomeSequence = new Sequence("ReturnHomeSequence");
+            returnHomeSequence.AddChild(new Leaf("WasChasing", new Stretegies.Condition(() => 
+                WasChasing && !EnemyDetected), 2));
+            returnHomeSequence.AddChild(new Leaf("ReturnHome", new Stretegies.ActionStrategy(Patrol), 1));
+            behaviorSelector.AddChild(returnHomeSequence);
+             
+            // Regular Patrol Logic
+            var patrolSequence = new Sequence("PatrolSelector");
+            patrolSequence.AddChild(new Leaf("CheckNotDetected", new Stretegies.Condition(() => !EnemyDetected), 1));
+            patrolSequence.AddChild(new Leaf("Patrol", new Stretegies.ActionStrategy(Patrol), 1));
+            behaviorSelector.AddChild(patrolSequence);
+            
+            _behaviourTree.AddChild(behaviorSelector);
+        }
+
+        private void Update()
+        {
+            if (GameStateManager.Instance.GameCurrentState != GameStateManager.GameState.OnGameplay)
+                return;
+            
+            _behaviourTree?.Process();
+            
+            if (Health <= 0)
+            {
+                Die();
+            }
+        }
+
+        public override void ReceiveDamage(float damage)
+        {
+            Animator.SetTrigger(DamagedHash);
+            Health -= damage;
+        }
+
+        private void Die()
+        {
+            Animator.SetTrigger(DieHash);
+            _behaviourTree = null;
+
+            if (Animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                ObjectPool.Instance.ReturnToPool(EnemyManager.Instance.idForEnemyPool, this.gameObject);
+            }
+        }
+
+        private void OnDisable()
+        {
+            GameStateManager.Instance.OnGameStateChanged -= OnStateChanged;
+        }
+    }
+
