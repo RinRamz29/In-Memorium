0f4012d50752cbeb9b8d92d4089f7efec72c0f60
RinRamz29 <142569946+RinRamz29@users.noreply.github.com>
2025-04-03 15:07:54 -0600

    ZoomController prefab

--- Assets/_Memoriam/Script/General/ZoomController.cs
+++ Assets/_Memoriam/Script/General/ZoomController.cs
@@ -1,56 +1,53 @@
 using System.Threading.Tasks;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Memoriam.Script.General
 {
     public class ZoomController : MonoBehaviour
     {
-        private CinemachineCamera _camera;
-        private float zoomIn = 2;
-        private float zoomOut = 4;
-        private float zoomSpeed = 1.5f;
+        [SerializeField] private CinemachineCamera cinemachineCamera;
+        [SerializeField] private float zoomIn = 2;
+        [SerializeField] private float zoomOut = 4;
+        [SerializeField] private float zoomSpeed = 1.5f;
 
-        private bool _IsZooming = false;
-
-        private void Awake()
-        {
-            _camera = GetComponent<CinemachineCamera>();
-        }
+        private bool _isZooming = false;
 
         private async void ChangeZoom(float target)
         {
-            if (_IsZooming) return;
-            _IsZooming = true;
+            if (_isZooming) 
+                return;
+            
+            _isZooming = true;
 
-            float startSize = _camera.Lens.OrthographicSize;
+            var startSize = cinemachineCamera.Lens.OrthographicSize;
             float elapsedTime = 0;
 
             while (elapsedTime < zoomSpeed)
             {
-                _camera.Lens.OrthographicSize = Mathf.Lerp(startSize, target, elapsedTime / zoomSpeed);
+                cinemachineCamera.Lens.OrthographicSize = Mathf.Lerp(startSize, target, elapsedTime / zoomSpeed);
                 elapsedTime += Time.deltaTime;
                 await Task.Yield();
             }
 
-            _camera.Lens.OrthographicSize = target;
-            _IsZooming = false;
+            cinemachineCamera.Lens.OrthographicSize = target;
+            _isZooming = false;
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.transform.TryGetComponent<Player.Player>(out var player))
             {
-                ChangeZoom(zoomIn);
+                if (cinemachineCamera.Lens.OrthographicSize <= zoomIn)
+                {
+                    ChangeZoom(zoomOut);
+                }
+                else
+                {
+                    ChangeZoom(zoomIn);
+                }
             }
         }
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            if (collision.transform.TryGetComponent<Player.Player>(out var player))
-            {
-                ChangeZoom(zoomOut);
-            }
-        }
-
     }
 }
