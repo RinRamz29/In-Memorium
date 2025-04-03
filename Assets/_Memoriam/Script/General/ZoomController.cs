c78400ddaef86491c7ac9e8abd05d759697f4e34
Ruben Arias <zap483@amerike.edu.mx>
2025-04-03 14:27:38 -0600

    Nose

--- Assets/_Memoriam/Script/General/ZoomController.cs
+++ Assets/_Memoriam/Script/General/ZoomController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Threading.Tasks;
 using Unity.Cinemachine;
 using UnityEngine;
@@ -6,50 +7,49 @@ namespace _Memoriam.Script.General
 {
     public class ZoomController : MonoBehaviour
     {
-        private CinemachineCamera _camera;
+        public CinemachineCamera _camera;
         private float zoomIn = 2;
         private float zoomOut = 4;
         private float zoomSpeed = 1.5f;
+         
 
-        private bool _IsZooming = false;
-
-        private void Awake()
-        {
-            _camera = GetComponent<CinemachineCamera>();
-        }
-
-        private async void ChangeZoom(float target)
-        {
-            if (_IsZooming) return;
-            _IsZooming = true;
-
-            float startSize = _camera.Lens.OrthographicSize;
-            float elapsedTime = 0;
-
-            while (elapsedTime < zoomSpeed)
-            {
-                _camera.Lens.OrthographicSize = Mathf.Lerp(startSize, target, elapsedTime / zoomSpeed);
-                elapsedTime += Time.deltaTime;
-                await Task.Yield();
-            }
-
-            _camera.Lens.OrthographicSize = target;
-            _IsZooming = false;
-        }
+        private Coroutine zoomCoroutine;
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.transform.TryGetComponent<Player.Player>(out var player))
+            if (collision.CompareTag("Zoom"))
             {
-                ChangeZoom(zoomIn);
+                if (zoomCoroutine != null)
+                    StopCoroutine(zoomCoroutine);
+
+               zoomCoroutine = StartCoroutine(ChangeZoom(zoomIn));
             }
         }
+
         private void OnTriggerExit2D(Collider2D collision)
         {
-            if (collision.transform.TryGetComponent<Player.Player>(out var player))
+            if (collision.CompareTag("Zoom"))
             {
-                ChangeZoom(zoomOut);
+                if (zoomCoroutine != null)
+                    StopCoroutine(zoomCoroutine);
+
+               zoomCoroutine = StartCoroutine(ChangeZoom(zoomOut));
             }
+        }
+
+        private IEnumerator ChangeZoom(float targetSize)
+        {
+            float startSize = _camera.Lens.OrthographicSize;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < zoomSpeed)
+            {
+                _camera.Lens.OrthographicSize = Mathf.Lerp(startSize, targetSize, elapsedTime / zoomSpeed);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            _camera.Lens.OrthographicSize = targetSize;
         }
 
     }
