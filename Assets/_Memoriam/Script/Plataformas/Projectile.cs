fa36abf4fff0da399f7ddc18d27ee916ff3ef59d
RinRamz29 <142569946+RinRamz29@users.noreply.github.com>
2025-04-03 14:53:01 -0600

    Projectile Launcher fixed

--- Assets/_Memoriam/Script/Plataformas/Projectile.cs
+++ Assets/_Memoriam/Script/Plataformas/Projectile.cs
@@ -12,39 +12,22 @@ namespace _Memoriam.Script.Plataformas
         [SerializeField] private string iDForPool;
         [SerializeField] private float projectileSpeed = 8f;
         [SerializeField] private Rigidbody2D rb;
-        [SerializeField] private float maxLifetime = 5f;
         public Vector2 Direction { get; set; }
-        
-        private float _spawnTime;
-
-        private void OnEnable()
-        {
-            _spawnTime = Time.time;
-        }
         
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.transform.TryGetComponent<IPlayer>(out var player))
             {
                 player.ReceiveDamage(damage, this.transform.position);
-                ObjectPool.Instance.ReturnToPool(iDForPool, gameObject);
             }
-            else if (!other.transform.TryGetComponent<BaseEnemy>(out var enem))
-            {
-                ObjectPool.Instance.ReturnToPool(iDForPool, this.gameObject);
-            }
+            
+            ObjectPool.Instance.ReturnToPool(iDForPool, this.gameObject);
         }
 
         private void Update()
         {
             // Apply velocity in the direction
             rb.linearVelocity = Direction * projectileSpeed;
-            
-            // Return to pool if lifetime exceeded
-            if (Time.time - _spawnTime > maxLifetime)
-            {
-                ObjectPool.Instance.ReturnToPool(iDForPool, gameObject);
-            }
         }
     }
 }
